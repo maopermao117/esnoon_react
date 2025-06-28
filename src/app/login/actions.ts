7009199afed91a0d76d3f0e1f9d3fd9836e76fb6
maopermao117 <maopermao117@gmail.com>
2025-06-29 03:59:13 +0900

    feature/Supabase-Auth-signup

---
+++ src/app/login/actions.ts
@@ -0,0 +1,48 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+import { createClient } from '@/utils/supabase/server'
+
+export async function login(formData: FormData) {
+  const supabase = await createClient()
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+
+  const { error } = await supabase.auth.signInWithPassword(data)  // これは分割代入してる.Supabaseがレスポンスでdata:{}とerror:を返すから完全一致で取り出してる.
+
+  if (error) {
+    redirect('/error')
+  }
+
+  revalidatePath('/', 'layout')
+  console.log("ログイン完了です")
+  redirect('/')
+}
+
+export async function signup(formData: FormData) {
+  const supabase = await createClient()
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+  }
+
+  const { error } = await supabase.auth.signUp(data)
+
+  if (error) {
+    redirect('/error')
+  }
+
+  console.log("新規登録成功です")
+  revalidatePath('/', 'layout')
+  redirect('/')
+}
